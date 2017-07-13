39dc2ba57b33cf3499b8148ff75a70d710ccec19
DeepSleepEngineer <deep-sleeper@nify.com>
2017-07-13 22:32:19 +0900

    Update EchoBack command source file. -Add comment of cmd_code00 function. -Add include file. -Change buffer index and return value from number into MACRO.

--- src/CMD/CMD_Code00.c
+++ src/CMD/CMD_Code00.c
@@ -1,9 +1,10 @@
 /**
- *  template.c
+ *  CMD_Code00.c
  *
- *  this is template file.
+ *  Source code of EchoBack command.
  */
 #include "ev3api.h"
+#include "CMD.h"
 
 /*****************************************************************************/
 /*                                外部変数宣言                               */
@@ -43,10 +44,11 @@ extern const int COMMAND_SEND_DATA_BUF_SIZE;
 /*                                  関数実装                                 */
 /*****************************************************************************/
 /**
- *  @brief  EchoBackコマンド本体
+ *  @brief  Main function of EchoBack command.
+ *          Check and analize command data and setup response data.
  */
 void cmd_code00(void) {
-    uint8_t res = 0x00;
+    uint8_t res = CMD_ERROR_OK;
     uint8_t cmd_data_len = 0x00;
     uint8_t res_data_len = 0x00;
     uint8_t sub_cmd_code = 0x00;
@@ -55,21 +57,21 @@ void cmd_code00(void) {
     uint8_t *snd_buf;
     int idx = 0;
 
-    rcv_buf = (uint8_t *)(&rcv_msg_buf[3]);
-    snd_buf = (uint8_t *)(&snd_msg_buf[4]);
+    rcv_buf = (uint8_t *)(&rcv_msg_buf[CMD_DATA_FORMAT_INDEX_CMD_DATA_TOP]);
+    snd_buf = (uint8_t *)(&snd_msg_buf[RES_DATA_FORMAT_INDEX_RES_DATA_TOP]);
     
     sub_cmd_code = rcv_msg_buf[1];
     cmd_data_len = rcv_msg_buf[2];
 
     sub_res_code = sub_cmd_code;
     if ((!(0 < cmd_data_len)) || (!(cmd_data_len < 30))) {
-        res = 0xFE;
+        res = CMD_ERROR_CMD_DATA_LEN;
     }
     if (0x00 != sub_cmd_code) {
-        res = 0xFD;
+        res = CMD_ERROR_INVALID_SUB_CODE;
         sub_res_code = 0xFF;
     }
-    if (0x00 == res) {
+    if (CMD_ERROR_OK == res) {
         for (idx = 0; idx < (int)cmd_data_len; idx++) {
             *snd_buf = *rcv_buf;
             snd_buf++;
@@ -80,9 +82,9 @@ void cmd_code00(void) {
         res_data_len = 0;
     }
     
-    snd_msg_buf[0] = 0x01;
-    snd_msg_buf[1] = sub_res_code;
-    snd_msg_buf[2] = res;
-    snd_msg_buf[3] = res_data_len;
+    snd_msg_buf[RES_DATA_FORMAT_INDEX_RES_CODE] = 0x01;
+    snd_msg_buf[RES_DATA_FORMAT_INDEX_RES_SUB_CODE] = sub_res_code;
+    snd_msg_buf[RES_DATA_FORMAT_INDEX_RES_CMD_RSLT] = res;
+    snd_msg_buf[RES_DATA_FORMAT_INDEX_RES_DATA_LEN] = res_data_len;
     snd_msg_len = 4 + res_data_len;
 }
