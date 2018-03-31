a63fcf7e32f51531672159496bf61717100994c0
CountrySideEngineer <deep-sleeper@nifty.com>
2018-03-31 19:31:05 +0900

    Update unit test of JUD module.

--- test/utest/UTEST_JUD/Debug/src/subdir.mk
+++ test/utest/UTEST_JUD/Debug/src/subdir.mk
@@ -8,11 +8,13 @@ CPP_SRCS += \
 
 C_SRCS += \
 /Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/dev/sdk/Ev3Car/src/JUD/JUD_DistSafeState.c \
-/Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/dev/sdk/Ev3Car/src/JUD/JUD_MotorOutputFailure.c 
+/Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/dev/sdk/Ev3Car/src/JUD/JUD_MotorOutputFailure.c \
+/Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/dev/sdk/Ev3Car/src/JUD/JUD_MotorOutputLimit.c 
 
 OBJS += \
 ./src/JUD_DistSafeState.o \
 ./src/JUD_MotorOutputFailure.o \
+./src/JUD_MotorOutputLimit.o \
 ./src/UTEST_JUD.o 
 
 CPP_DEPS += \
@@ -20,7 +22,8 @@ CPP_DEPS += \
 
 C_DEPS += \
 ./src/JUD_DistSafeState.d \
-./src/JUD_MotorOutputFailure.d 
+./src/JUD_MotorOutputFailure.d \
+./src/JUD_MotorOutputLimit.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -32,6 +35,13 @@ src/JUD_DistSafeState.o: /Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/dev/sd
 	@echo ' '
 
 src/JUD_MotorOutputFailure.o: /Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/dev/sdk/Ev3Car/src/JUD/JUD_MotorOutputFailure.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I"/Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/test/utest/tools/google_test/include" -I"/Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/test/utest/../../dev/sdk/Ev3Car/src" -I"/Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/test/utest/tools/include" -include"/Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/test/utest/tools/include/ev3api_utest_typedef.h" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/JUD_MotorOutputLimit.o: /Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/dev/sdk/Ev3Car/src/JUD/JUD_MotorOutputLimit.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
 	gcc -I"/Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/test/utest/tools/google_test/include" -I"/Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/test/utest/../../dev/sdk/Ev3Car/src" -I"/Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/test/utest/tools/include" -include"/Volumes/USB_DEV/ev3_toppers_app/Ev3MindStormCar/test/utest/tools/include/ev3api_utest_typedef.h" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
--- test/utest/UTEST_JUD/src/UTEST_JUD.cpp
+++ test/utest/UTEST_JUD/src/UTEST_JUD.cpp
@@ -19,6 +19,7 @@ int left_motor_power_hys;
 int right_motor_power_hys;
 int target_motor_output_left;
 int target_motor_output_right;
+int8_t distance_safe_state;
 
 extern int8_t distance_safe_state;
 extern int8_t distance_safe_state_prev;
@@ -29,6 +30,7 @@ extern int16_t motor_failure_left_count;
 extern int16_t motor_failure_right_count;
 extern uint8_t motor_output_diff_left;
 extern uint8_t motor_output_diff_right;
+extern int8_t motor_output_max;
 
 extern const int8_t CAR_SAFE_STATE_SAFE;
 extern const int8_t CAR_SAFE_STATE_ATTN;
@@ -36,12 +38,18 @@ extern const int8_t CAR_SAFE_STATE_DANG;
 extern const int8_t CAR_SAFE_STATE_STOP;
 extern const int16_t MOTOR_FAILURE_COUNT;
 extern const int8_t MOTOR_FAILURE_OUTPUT;
+extern const int8_t MOTOR_OUTPUT_LIMIT_STATE_SAFE;
+extern const int8_t MOTOR_OUTPUT_LIMIT_STATE_ATTN;
+extern const int8_t MOTOR_OUTPUT_LIMIT_STATE_DANG;
+extern const int8_t MOTOR_OUTPUT_LIMIT_STATE_STOP;
 
 extern void judge_dist_safe(void);
 extern void judge_distance_safe_change(void);
 extern void init_dist_safe(void);
 extern void init_motor_output_failure(void);
 extern void judge_motor_output_failure(void);
+extern void init_motor_output_limit(void);
+extern void judge_motor_output_limit(void);
 #ifdef __cplusplus
 }
 #endif
@@ -324,4 +332,58 @@ TEST(judge_dist_safe_state, judge_motor_output_failure_006) {
 	EXPECT_EQ(1, motor_failure_left);
 	EXPECT_EQ(MOTOR_FAILURE_COUNT - 1, motor_failure_right_count);
 	EXPECT_EQ(1, motor_failure_right);
+}
+
+TEST(judge_dist_safe_state, init_motor_output_limit_001) {
+
+	motor_output_max = MOTOR_OUTPUT_LIMIT_STATE_DANG;
+
+	init_motor_output_limit();
+
+	EXPECT_EQ(MOTOR_OUTPUT_LIMIT_STATE_SAFE, motor_output_max);
+}
+
+TEST(judge_dist_safe_state, judge_motor_output_limit_001) {
+	distance_safe_state = CAR_SAFE_STATE_SAFE;
+	motor_output_max = MOTOR_OUTPUT_LIMIT_STATE_STOP;
+
+	judge_motor_output_limit();
+
+	EXPECT_EQ(MOTOR_OUTPUT_LIMIT_STATE_SAFE, motor_output_max);
+}
+
+TEST(judge_dist_safe_state, judge_motor_output_limit_002) {
+	distance_safe_state = CAR_SAFE_STATE_ATTN;
+	motor_output_max = MOTOR_OUTPUT_LIMIT_STATE_STOP;
+
+	judge_motor_output_limit();
+
+	EXPECT_EQ(MOTOR_OUTPUT_LIMIT_STATE_ATTN, motor_output_max);
+}
+
+TEST(judge_dist_safe_state, judge_motor_output_limit_003) {
+	distance_safe_state = CAR_SAFE_STATE_DANG;
+	motor_output_max = MOTOR_OUTPUT_LIMIT_STATE_STOP;
+
+	judge_motor_output_limit();
+
+	EXPECT_EQ(MOTOR_OUTPUT_LIMIT_STATE_DANG, motor_output_max);
+}
+
+TEST(judge_dist_safe_state, judge_motor_output_limit_004) {
+	distance_safe_state = CAR_SAFE_STATE_STOP;
+	motor_output_max = MOTOR_OUTPUT_LIMIT_STATE_ATTN;
+
+	judge_motor_output_limit();
+
+	EXPECT_EQ(MOTOR_OUTPUT_LIMIT_STATE_STOP, motor_output_max);
+}
+
+TEST(judge_dist_safe_state, judge_motor_output_limit_005) {
+	distance_safe_state = CAR_SAFE_STATE_STOP + 1;
+	motor_output_max = MOTOR_OUTPUT_LIMIT_STATE_ATTN;
+
+	judge_motor_output_limit();
+
+	EXPECT_EQ(MOTOR_OUTPUT_LIMIT_STATE_STOP, motor_output_max);
 }
